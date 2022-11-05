3a8d86b8be18aa060739bc85dbfe51013b4b8dc3
yanyangtian1106 <1035282452@qq.com>
2022-11-05 19:34:35 +0800

    处理代码异常编程题

---
+++ 处理异常编程题.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_sharp_笔记
+{
+    class 处理异常编程题
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("请输入两个数字,每行一个");
+
+            int n1 = 0;
+            int n2 = 0;
+            //try
+            //{
+            // n1 = Convert.ToInt32(Console.ReadLine());
+            // n2 = Convert.ToInt32(Console.ReadLine());
+            //}
+            //catch(FormatException e)//出现格式化异常，说明用户输入非法数据
+            //{
+            //    Console.WriteLine("你输入的数据不符合规则，请重新输入");
+            //     n1 = Convert.ToInt32(Console.ReadLine());
+            //     n2 = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            while(true)
+            {
+                try//如果出现问题就进入catch，如果不出现问题就不用进入catch
+                {
+                    n1 = Convert.ToInt32(Console.ReadLine());
+                    n2 = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException e)//出现格式化异常，说明用户输入非法数据
+                {
+                    Console.WriteLine("你输入的数据不符合规则，请重新输入");
+                }
+            }
+            Console.WriteLine(n1 + n2);
+        }
+    }
+}
