0185f175fdc0749a3819dfa26984294626e6d110
yanyangtian1106 <1035282452@qq.com>
2022-11-09 18:29:53 +0800

    publicAndprivate

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace publicAndpribate
+{
+
+    class Vector3
+    {
+        private float x;
+        private float y;
+        private float z;
+
+        public void SetX(float temp)
+        {
+            if (temp <= 0)
+            {
+                return;
+            }
+            x = temp;
+        }
+        public void SetY(float temp)
+        {
+            if (temp <= 0)
+            {
+                return;
+            }
+            y = temp;
+        }
+        public void SetZ(float temp)
+        {
+            if (temp <= 0)
+            {
+                return;
+            }
+            z = temp;
+        }
+        public double Length()
+        {
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace publicAndpribate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector3 V1 = new Vector3();
+            V1.SetX(3.4f);
+            V1.SetY(3.2f);
+            V1.SetZ(5.2f);
+            Console.WriteLine(V1.Length());
+        }
+    }
+}
