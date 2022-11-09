c899ec4e661ecd056bbfa490e722204b5d923c62
yanyangtian1106 <1035282452@qq.com>
2022-11-09 18:23:06 +0800

    构造函数

---
+++ 构造函数/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 构造函数
+{
+    class Class1
+    {
+        public string name;
+        public string address;
+        public int age;
+        public string creatTime;
+        public Class1()
+        {
+            Console.WriteLine("我是构造函数");
+        }
+        public Class1(string arg1,string arg2,int arg3,string arg4)
+        {
+            name = arg1;
+            address = arg2;
+            age = arg3;
+            creatTime = arg4;
+        }
+        public void Show()
+        {
+            Console.WriteLine("姓名：" + name);
+            Console.WriteLine("地址：" + address);
+            Console.WriteLine("年龄:" + age);
+            Console.WriteLine("创建时间：" + creatTime);
+        }
+    } 
+
+}
---
+++ 构造函数/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 构造函数//用来初始化数据,无返回值
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 str = new Class1();//有多个构造函数时，构造函数会自己调用对应匹配的函数
+            str.Show();
+
+
+            Class1 yyt = new Class1("晏阳天", "双流", 20, "2021年");
+            yyt.Show();
+
+        }
+    }
+}
